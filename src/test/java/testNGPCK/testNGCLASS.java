6f36456170b28f0608415b44a0df42f26d065957
Beesam <Shilpa.Beesam@parexel.com>
2020-01-18 21:21:12 +0530

    new branch and applying changes

--- src/test/java/testNGPCK/testNGCLASS.java
+++ src/test/java/testNGPCK/testNGCLASS.java
@@ -26,7 +26,7 @@ public class testNGCLASS {
 	
 	@BeforeMethod
 	public void launchBrowser() {
-		System.out.println("launching firefox browser for the 1st time");
+		System.out.println("launching firefox browser for the 2nd time");
 		System.setProperty("webdriver.gecko.driver", driverPath);
 		driver = new FirefoxDriver();
 		driver.get(baseUrl);
